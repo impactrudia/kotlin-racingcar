3bbc18d20053b43a7a30b0eb84639c27cffd99b4
impactrudia <impactyoung@gmail.com>
2023-06-26 10:23:45 +0900

    review :  홀수 짝수 불분명한 매직넘버 및 rem 대신 직접 연산자 호출

--- src/main/kotlin/calculrate/Calculator.kt
+++ src/main/kotlin/calculrate/Calculator.kt
@@ -2,15 +2,20 @@ package calculrate
 
 object Calculator {
 
+    private const val isRemainderEven = 0
+    private const val isRemainderOdd = 1
+
     fun calculate(expression: String?): Int {
         require(!expression.isNullOrBlank())
 
         val splits = expression.split(" ")
-        val numbers = splits.filterIndexed { index, _ -> index.rem(2) == 0 }.map { it.toInt() }
-        val operators = splits.filterIndexed { index, _ -> index.rem(2) == 1 }.map { Operator.of(it) }
+        val numbers = splits.filterIndexed { index, _ -> isEvenOrOdd(index, isRemainderEven) }.map { it.toInt() }
+        val operators = splits.filterIndexed { index, _ -> isEvenOrOdd(index, isRemainderOdd) }.map { Operator.of(it) }
 
         return calculate(numbers, operators.toMutableList())
     }
+
+    private fun isEvenOrOdd(index: Int, remainNumber: Int) = index % 2 == remainNumber
 
     private fun calculate(numbers: List<Int>, operators: MutableList<Operator>): Int {
         return numbers.reduce { first, second ->
